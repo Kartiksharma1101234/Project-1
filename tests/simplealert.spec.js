f03a0a15633d5c604a43720771edb763ca4835f4
Kartik Sharma <kartik037@Loan112.com>
2026-06-17 18:27:53 +0530

    Simple alert

---
+++ tests/simplealert.spec.js
@@ -0,0 +1,15 @@
+const{test,expect} = require("@playwright/test")
+test("Check the simple alert", async function ({page}) {
+    await page.goto("https://the-internet.herokuapp.com/")
+
+    await page.locator("//a[normalize-space()='JavaScript Alerts']").click()
+
+    await expect(page).toHaveURL("https://the-internet.herokuapp.com/javascript_alerts")
+
+    page.on('dialog', async dialog=>{
+        console.log(dialog.message())
+        await dialog.accept()
+    })
+    await page.locator("//button[normalize-space()='Click for JS Alert']")
+    
+})
